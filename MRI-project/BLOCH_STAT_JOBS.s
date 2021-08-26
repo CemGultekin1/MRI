3d78d16aaef60f5cb4eefbedb5b1f800bd6db847
CemGultekin1 <cg3306@nyu.edu>
2021-08-26 13:40:34 +0300

    mri update

--- MRI-project/BLOCH_STAT_JOBS.s
+++ MRI-project/BLOCH_STAT_JOBS.s
@@ -7,11 +7,11 @@
 #SBATCH --mem=50GB
 #SBATCH --output=job_stat_%a.out
 #SBATCH --error=job_stat_%a.err
-#SBATCH --array=23-26,38-40
 
 module purge
 module load matlab/2020b
-if[%SLURM_ARRAY_TASK_ID%<38]; then
+if[%SLURM_ARRAY_TASK_ID%<38]
+then
 matlab -nodesktop -singleCompThread -r  "server_bloch_multi_statistics(${SLURM_ARRAY_TASK_ID},5,'tag=m0s0p1')" < /dev/null
 matlab -nodesktop -singleCompThread -r  "server_bloch_multi_statistics(${SLURM_ARRAY_TASK_ID},5,'')" < /dev/null
 else
